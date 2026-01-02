09cd1f56d85ad99257cbc30c6766a3329ce01204
Wanjavwa <wanjavwa25@gmail.com>
2026-01-02 03:27:14 -0500

    Add files via upload

---
+++ our_mobile_applicationTests.swift
@@ -0,0 +1,16 @@
+//
+//  our_mobile_applicationTests.swift
+//  our mobile applicationTests
+//
+//  Created by Wanjavwa Nzobokela on 7/20/25.
+//
+
+import Testing
+
+struct our_mobile_applicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
